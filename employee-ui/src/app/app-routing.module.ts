61d0c9161a64d85072e512786d2fda7cb8a007c8
SB karunarathne <karunarathne.ucsc@gmail.com>
2023-10-16 21:08:00 +0530

    Adding new UI components for employee page

--- employee-ui/src/app/app-routing.module.ts
+++ employee-ui/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EmployeesComponent } from './components/employees/employees.component';
+import { Error404Component } from './components/error404/error404.component';
+import { AuthGuard } from './guards/auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'employees', pathMatch: 'full' },
+  { path: 'employees', component: EmployeesComponent, canActivate: [AuthGuard] },
+  { path: '**', component: Error404Component }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- employee-ui/src/app/app.module.ts
+++ employee-ui/src/app/app.module.ts
@@ -3,16 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmployeesComponent } from './components/employees/employees.component';
+import { Error404Component } from './components/error404/error404.component';
+import { AuthGuard } from './guards/auth.guard';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeesComponent,
+    Error404Component
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
