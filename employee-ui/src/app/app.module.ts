d804e6512c67a8967d179b75ae1170c852be7190
SB karunarathne <karunarathne.ucsc@gmail.com>
2023-10-16 21:52:58 +0530

    Added the basic employee components

--- employee-ui/src/app/app.module.ts
+++ employee-ui/src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppComponent } from './app.component';
 import { EmployeesComponent } from './components/employees/employees.component';
 import { Error404Component } from './components/error404/error404.component';
 import { AuthGuard } from './guards/auth.guard';
+import { EmployeeListComponent } from './components/employees/employee-list.component';
+import { EmployeeComponent } from './components/employees/employee.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EmployeesComponent,
-    Error404Component
+    Error404Component,
+    EmployeeListComponent,
+    EmployeeComponent
   ],
   imports: [
     BrowserModule,
